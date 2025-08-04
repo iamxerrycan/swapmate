5c4524772b9825622e4bf07f8650afd1977b6a6b
iamxerrycan <rajshish333@gmail.com>
2025-08-04 23:04:13 +0530

    api isssue

--- frontend/src/pages/Dashboard/MyProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-export default function MyProfilePage() {
-  return (
-    <div>
-      <h1>My Profile</h1>
-      <p>Your admin profile details.</p>
-    </div>
-  );
-}
--- frontend/src/pages/admin/UsersPage.jsx
+++
@@ -1,9 +0,0 @@
-//  src/pages/admin/UsersPage.jsx
-export default function UsersPage() {
-  return (
-    <div>
-      <h1>Manage Users</h1>
-      <p>List of users will appear here.</p>
-    </div>
-  );
-}
